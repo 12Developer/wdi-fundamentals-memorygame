0ba70ba134a3efa637061f9abade2af881954bba
Macbook Pro <MacbookPro@MacBook.local>
2018-01-21 22:49:27 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = ["queen", "king", "queen", "king"]
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+(cardsInPlay[0] === cardsInPlay[1])
+alert("You found a match!");
+} else {
+
+(cardsInPlay[0] !== cardsInPlay[1])
+alert("Sorry, try again.");
+}
