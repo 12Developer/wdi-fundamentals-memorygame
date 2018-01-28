a361bbd5adb54212381b60f60f8d261305c622ef
CorderoDixon <CorderoDixon12@icloud.com>
2018-01-27 19:54:32 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,43 +1,46 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards= [
+    { 
+    rank: "queen",
+    suit: "hearts",
+    <img src="images/queen-of-hearts.png">
+    },
+    {
+    rank: "queen",
+    suit: "diamonds",
+    <img src="images/queen-of-diamonds.png">
+    },
+    {
+    rank: "king",
+    suit: "hearts",
+    <img src= "images/king-of-hearts.png">
+    },
+    {
+    rank: "king",
+    suit: "diamonds",
+    <img src= "images/king-of-diamonds.png">   
+    }
+    ];
 
 var cardsInPlay = [    ];
 
-
 (cardsInPlay.length === 2); 
 
-checkForMatch = function(if (cardsInPlay[0]] === cardsInPlay[1]) {
+checkForMatch = function( 
+    if (cardsInPlay[0]] === cardsInPlay[1]) {
 alert("You found a match!");
 } else {
 alert("Sorry, try again.");
 })
 
-flipCard = function(checkForMatch);
-console.log("User flipped" + cards[0]);
-cardsInPlay.push(card[0]);
+var flipCard = function(checkForMatch);
+console.log("User flipped" + cards[0].rank + cards[0].cardImage + cards[0].suit);
+console.log("User flipped" + cards[1].rank + cards[1].cardImage + cards[1].suit);
+console.log("User flipped" + cards[2].rank + cards[2].cardImage + cards[2].suit);
+console.log("User flipped" + cards[3].rank + cards[3].cardImage + cards[3].suit);
+
+cardsInPlay.push(cards[0].rank);
+cardsInPlay.push(cards[2].rank);
 flipCard(0);
 flipCard(2);
-
-var cardImage = [
-{ 
-rank: "queen",
-suit: "hearts",
-"images/queen-of-hearts.png"
-},
-{
-rank: "queen",
-suit: "diamonds",
-"images/queen-of-diamonds.png"
-},
-{
-rank: "king",
-suit: "hearts",
-"images/king-of-hearts.png"
-},
-{
-rank: "king",
-suit: "diamonds",
-"images/king-of-diamonds.png"    
-}
-];
