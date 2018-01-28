b08fbf5c8161cb80db28440ad6a3d0e69b8c737e
CorderoDixon <CorderoDixon12@icloud.com>
2018-01-27 19:25:46 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,22 +2,42 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = ["queen", "king", "queen", "king"]
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var cardsInPlay = [    ];
 
-cardsInPlay.push("queen");
-cardsInPlay.push("king");
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+(cardsInPlay.length === 2); 
 
-if (cardsInPlay.length === 2) {
-
-(cardsInPlay[0] === cardsInPlay[1])
+checkForMatch = function(if (cardsInPlay[0]] === cardsInPlay[1]) {
 alert("You found a match!");
 } else {
-
-(cardsInPlay[0] !== cardsInPlay[1])
 alert("Sorry, try again.");
+})
+
+flipCard = function(checkForMatch);
+console.log("User flipped" + cards[0]);
+cardsInPlay.push(card[0]);
+flipCard(0);
+flipCard(2);
+
+var cardImage = [
+{ 
+rank: "queen",
+suit: "hearts",
+"images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+"images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+"images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+"images/king-of-diamonds.png"    
 }
+];
